05bb4537a96f7398221ef8afd722bfb256e4c060
haymi_g <haymig75@gmail.com>
2026-03-11 19:50:04 +0300

    codforce contest solution

---
+++ C_Book_Ordering.py
@@ -0,0 +1,11 @@
+# n = int(input())
+# max_count = 0
+# for _ in range(n):
+#   w , h = map(int , input().split())
+#   if h > max_count and h < 
+# #   if w < h:
+# #     count += 1
+# # if count  == n :
+# #   print("YES")
+# # else:
+# #   print("NO")
